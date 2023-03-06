a0c5176f3a8db239c3c337d32b20f8daca01dc33
@pinndulum <jemecham@gmail.com>
2023-03-06 00:42:13 -0700

    mat slider error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,6 +37,7 @@ import { TimePipe } from './pipes/time.pipe';
     HttpClientModule,
     MatButtonModule,
     MatDialogModule,
+    MatSelectModule,
     MatSliderModule,
     BrowserModule,
     BrowserAnimationsModule,
