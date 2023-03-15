781d6439451db6ec9d5613ea23bb8964b67b6c43
@pinndulum <jemecham@gmail.com>
2023-03-14 18:19:49 -0600

    return of the slider

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -15,6 +15,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+// import { MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
@@ -23,12 +24,14 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+// import { DomSanitizer } from '@angular/platform-browser';
 
 @NgModule({
     imports: [
@@ -55,8 +58,9 @@ import { MatTreeModule } from '@angular/material/tree';
         MatRadioModule,
         MatSelectModule,
         MatSidenavModule,
+        MatSliderModule,
         MatSlideToggleModule,
-        // MatSnackBarModule,
+        MatSnackBarModule,
         MatTabsModule,
         MatToolbarModule,
         MatTooltipModule,
@@ -89,8 +93,9 @@ import { MatTreeModule } from '@angular/material/tree';
         MatRadioModule,
         MatSelectModule,
         MatSidenavModule,
+        MatSliderModule,
         MatSlideToggleModule,
-        // MatSnackBarModule,
+        MatSnackBarModule,
         MatTabsModule,
         MatToolbarModule,
         MatTooltipModule,
